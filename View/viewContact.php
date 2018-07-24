0c6c96ed5d221b1cc5e12e77601bb04cc7e1629e
Adeline <deca.adeline@gmail.com>
2018-07-24 15:17:59 +0200

    léger correctif pour l'affichge du titre de viewContact

--- View/viewContact.php
+++ View/viewContact.php
@@ -1,4 +1,5 @@
-<?php $this->titre = 'Billet simple pour l\'Alaska - Jean Forteroche'; ?>
+<?php $this->title = 'Billet simple pour l\'Alaska - Jean Forteroche'; ?>
+
 <div class="col-xs-offset-1 col-xs-10 contact">
     <h2>A propos de moi</h2>
     <p>
