350d982ad49cc71b81dd0ad78a3b943e71beae9d
Adeline <deca.adeline@gmail.com>
2018-07-30 21:18:19 +0200

    mise à jour de la bdd

--- p8.sql
+++ p8.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Client :  127.0.0.1:3306
--- Généré le :  Jeu 26 Juillet 2018 à 10:29
+-- Généré le :  Lun 30 Juillet 2018 à 19:17
 -- Version du serveur :  5.6.34-log
 -- Version de PHP :  7.0.13
 
@@ -28,10 +28,17 @@ SET time_zone = "+00:00";
 
 CREATE TABLE IF NOT EXISTS `admin` (
   `id` int(11) NOT NULL,
-  `name` varchar(255) NOT NULL,
+  `login` varchar(255) NOT NULL,
   `pass` varchar(255) NOT NULL,
   `email` varchar(255) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=latin1;
+
+--
+-- Contenu de la table `admin`
+--
+
+INSERT INTO `admin` (`id`, `login`, `pass`, `email`) VALUES
+(1, 'admin', '$2y$10$CmdircekHV59Hy7Me63ahuRN1lPPVw4ZBmihdkTKh7Zo9kJSIncli', 'deca.adeline@gmail.com');
 
 -- --------------------------------------------------------
 
@@ -44,27 +51,28 @@ CREATE TABLE IF NOT EXISTS `comments` (
   `post_id` int(11) NOT NULL,
   `author` varchar(255) NOT NULL,
   `comment` text NOT NULL,
+  `reported` tinyint(1) NOT NULL,
   `comment_date` datetime NOT NULL
-) ENGINE=InnoDB AUTO_INCREMENT=14 DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB AUTO_INCREMENT=20 DEFAULT CHARSET=latin1;
 
 --
 -- Contenu de la table `comments`
 --
 
-INSERT INTO `comments` (`id`, `post_id`, `author`, `comment`, `comment_date`) VALUES
-(1, 1, 'Jean', 'Je teste les commentaires', '2018-07-23 15:35:00'),
-(2, 1, 'moi', 'teste de commentaire', '2018-07-24 07:44:11'),
-(3, 1, 'moi', 'teste de commentaire 2', '2018-07-24 08:44:11'),
-(4, 1, 'moi', 'teste de commentaire 3', '2018-07-24 09:44:11'),
-(5, 1, 'moi', 'teste de commentaire 4', '2018-07-24 10:44:11'),
-(6, 1, 'moi', 'teste de commentaire 5', '2018-07-24 11:44:11'),
-(7, 1, 'moi', 'teste de commentaire 6', '2018-07-24 12:44:11'),
-(8, 1, 'moi', 'teste de commentaire 7', '2018-07-25 06:44:11'),
-(9, 1, 'moi', 'teste de commentaire 8', '2018-07-25 06:46:52'),
-(10, 1, 'moi', 'teste de commentaire 9', '2018-07-25 07:44:11'),
-(11, 1, 'moi', 'teste de commentaire 10', '2018-07-25 08:44:11'),
-(12, 1, 'moi', 'teste de commentaire 11', '2018-07-25 09:44:11'),
-(13, 1, 'moi', 'teste de commentaire 12', '2018-07-25 10:44:11');
+INSERT INTO `comments` (`id`, `post_id`, `author`, `comment`, `reported`, `comment_date`) VALUES
+(1, 1, 'Jean', 'Je teste les commentaires', 0, '2018-07-23 15:35:00'),
+(2, 1, 'moi', 'teste de commentaire', 0, '2018-07-24 07:44:11'),
+(3, 1, 'moi', 'teste de commentaire 2', 0, '2018-07-24 08:44:11'),
+(5, 1, 'moi', 'teste de commentaire 4', 0, '2018-07-24 10:44:11'),
+(6, 1, 'moi', 'teste de commentaire 5', 0, '2018-07-24 11:44:11'),
+(7, 1, 'moi', 'teste de commentaire 6', 0, '2018-07-24 12:44:11'),
+(8, 1, 'moi', 'teste de commentaire 7', 0, '2018-07-25 06:44:11'),
+(9, 1, 'moi', 'teste de commentaire 8', 0, '2018-07-25 06:46:52'),
+(10, 1, 'moi', 'teste de commentaire 9', 0, '2018-07-25 07:44:11'),
+(11, 1, 'moi', 'teste de commentaire 10', 0, '2018-07-25 08:44:11'),
+(12, 1, 'moi', 'teste de commentaire 11\r\n(modéré)', 2, '2018-07-25 09:44:11'),
+(18, 4, 'je teste', 'test ', 0, '2018-07-27 09:58:06'),
+(19, 5, 'Test', 'Je teste la modération !', 2, '2018-07-27 13:44:35');
 
 -- --------------------------------------------------------
 
@@ -78,7 +86,7 @@ CREATE TABLE IF NOT EXISTS `posts` (
   `content` text NOT NULL,
   `author` varchar(255) NOT NULL,
   `publication_date` datetime NOT NULL
-) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB AUTO_INCREMENT=9 DEFAULT CHARSET=latin1;
 
 --
 -- Contenu de la table `posts`
@@ -89,8 +97,8 @@ INSERT INTO `posts` (`id`, `title`, `content`, `author`, `publication_date`) VAL
 (2, 'Test 2', 'Post hoc impie perpetratum quod in aliis quoque iam timebatur, tamquam licentia crudelitati indulta per suspicionum nebulas aestimati quidam noxii damnabantur. quorum pars necati, alii puniti bonorum multatione actique laribus suis extorres nullo sibi relicto praeter querelas et lacrimas, stipe conlaticia victitabant, et civili iustoque imperio ad voluntatem converso cruentam, claudebantur opulentae domus et clarae.\r\n', 'Jean Forteroche', '2018-07-23 19:30:20'),
 (3, 'Test 3\r\n', 'Post hoc impie perpetratum quod in aliis quoque iam timebatur, tamquam licentia crudelitati indulta per suspicionum nebulas aestimati quidam noxii damnabantur. quorum pars necati, alii puniti bonorum multatione actique laribus suis extorres nullo sibi relicto praeter querelas et lacrimas, stipe conlaticia victitabant, et civili iustoque imperio ad voluntatem converso cruentam, claudebantur opulentae domus et clarae.\r\n', 'Jean Forteroche', '2018-07-24 07:30:20'),
 (4, 'Test 4\r\n', 'Post hoc impie perpetratum quod in aliis quoque iam timebatur, tamquam licentia crudelitati indulta per suspicionum nebulas aestimati quidam noxii damnabantur. quorum pars necati, alii puniti bonorum multatione actique laribus suis extorres nullo sibi relicto praeter querelas et lacrimas, stipe conlaticia victitabant, et civili iustoque imperio ad voluntatem converso cruentam, claudebantur opulentae domus et clarae.\r\n', 'Jean Forteroche', '2018-07-25 06:30:20'),
-(5, 'Test 5\r\n', 'Post hoc impie perpetratum quod in aliis quoque iam timebatur, tamquam licentia crudelitati indulta per suspicionum nebulas aestimati quidam noxii damnabantur. quorum pars necati, alii puniti bonorum multatione actique laribus suis extorres nullo sibi relicto praeter querelas et lacrimas, stipe conlaticia victitabant, et civili iustoque imperio ad voluntatem converso cruentam, claudebantur opulentae domus et clarae.\r\n', 'Jean Forteroche', '2018-07-25 07:30:20'),
-(6, 'Test 6\r\n', 'Post hoc impie perpetratum quod in aliis quoque iam timebatur, tamquam licentia crudelitati indulta per suspicionum nebulas aestimati quidam noxii damnabantur. quorum pars necati, alii puniti bonorum multatione actique laribus suis extorres nullo sibi relicto praeter querelas et lacrimas, stipe conlaticia victitabant, et civili iustoque imperio ad voluntatem converso cruentam, claudebantur opulentae domus et clarae.\r\n', 'Jean Forteroche', '2018-07-25 07:36:20');
+(5, 'Test 5 (modifié) !', 'Post hoc impie perpetratum quod in aliis quoque iam timebatur, tamquam licentia crudelitati indulta per suspicionum nebulas aestimati quidam noxii damnabantur. quorum pars necati, alii puniti bonorum multatione actique laribus suis extorres nullo sibi relicto praeter querelas et lacrimas, stipe conlaticia victitabant, et civili iustoque imperio ad voluntatem converso cruentam, claudebantur opulentae domus et clarae.\r\n', 'Jean Forteroche', '2018-07-25 07:30:20'),
+(7, 'Test TinyMce', '<p>Nisi mihi Phaedrum, inquam, tu mentitum aut <strong>Zenonem</strong> putas, quorum utrumque audivi, cum mihi nihil sane praeter sedulitatem probarent, omnes mihi Epicuri sententiae satis notae sunt. atque eos, quos nominavi, cum Attico nostro frequenter audivi, cum miraretur ille quidem utrumque, Phaedrum autem etiam amaret, cotidieque inter nos ea, quae audiebamus, <em>conferebamus, neque erat umquam controversia, quid ego intellegerem, sed quid probarem.</em></p>\r\n<p style="text-align: center;"><br />Altera sententia est, quae definit amicitiam paribus officiis ac voluntatibus. Hoc quidem est nimis exigue et exiliter ad calculos vocare amicitiam, ut par sit ratio acceptorum et datorum. Divitior mihi et affluentior videtur esse vera amicitia nec observare restricte, ne plus reddat quam acceperit; neque enim verendum est, ne quid excidat, aut ne quid in terram defluat, aut ne plus aequo quid in amicitiam <span style="color: #ff0000;">congeratur.</span></p>\r\n<h3><br /><span style="text-decoration: underline;">Mox dicta finierat</span>, multitudo omnis ad, quae imperator voluit, promptior <span style="background-color: #ccffff;">laudato</span> consilio consensit in pacem ea ratione maxime percita, quod norat expeditionibus crebris fortunam eius in malis tantum civilibus vigilasse, cum autem bella moverentur externa, accidisse plerumque luctuosa, icto post haec foedere gentium ritu perfectaque sollemnitate imperator Mediolanum ad hiberna <span style="text-decoration: line-through;">discessit</span>.</h3>\r\n<div id="s3gt_translate_tooltip_mini" class="s3gt_translate_tooltip_mini_box" style="background: initial !important; border: initial !important; border-radius: initial !important; border-spacing: initial !important; border-collapse: initial !important; direction: ltr !important; flex-direction: initial !important; font-weight: initial !important; height: initial !important; letter-spacing: initial !important; min-width: initial !important; max-width: initial !important; min-height: initial !important; max-height: initial !important; margin: auto !important; outline: initial !important; padding: initial !important; position: absolute; table-layout: initial !important; text-align: initial !important; text-shadow: initial !important; width: initial !important; word-break: initial !important; word-spacing: initial !important; overflow-wrap: initial !important; box-sizing: initial !important; display: initial !important; color: inherit !important; font-size: 13px !important; font-family: X-LocaleSpecific, sans-serif, Tahoma, Helvetica !important; line-height: 13px !important; vertical-align: top !important; white-space: inherit !important; left: 515px; top: 284px; opacity: 0.55;">\r\n<div id="s3gt_translate_tooltip_mini_logo" class="s3gt_translate_tooltip_mini" title="Traduire le texte s&eacute;lectionn&eacute;">&nbsp;</div>\r\n<div id="s3gt_translate_tooltip_mini_sound" class="s3gt_translate_tooltip_mini" title="Lecture">&nbsp;</div>\r\n<div id="s3gt_translate_tooltip_mini_copy" class="s3gt_translate_tooltip_mini" title="Copier le texte dans le presse-papiers">&nbsp;</div>\r\n</div>', 'Moi', '2018-07-28 08:59:59');
 
 --
 -- Index pour les tables exportées
@@ -122,17 +130,17 @@ ALTER TABLE `posts`
 -- AUTO_INCREMENT pour la table `admin`
 --
 ALTER TABLE `admin`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=2;
 --
 -- AUTO_INCREMENT pour la table `comments`
 --
 ALTER TABLE `comments`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=14;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=20;
 --
 -- AUTO_INCREMENT pour la table `posts`
 --
 ALTER TABLE `posts`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=7;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=9;
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
