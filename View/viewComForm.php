2ec4c81305acdbb82593a51b920afe8be359cf98
Adeline <deca.adeline@gmail.com>
2018-07-31 19:03:34 +0200

    css moderation commentaires

--- View/viewComForm.php
+++ View/viewComForm.php
@@ -1,4 +1,4 @@
-<?php $this->titre = "Supression d'un commentaire";
+<?php $this->title = "Modération d'un commentaire";
 if (isset($_SESSION['id']) AND isset($_SESSION['login']))
     {
 ?>
@@ -12,18 +12,18 @@ if (isset($_SESSION['id']) AND isset($_SESSION['login']))
         }
     ?>
     <div class="col-lg-offset-3 col-lg-6 col-xs-offset-1 col-xs-10 newCom">
-        <h2>Moderer le commentaire</h2>
         <form action="index.php?action=modifyCom" method="post" class="col-xs-offset-2 col-xs-8 col-sm-offset-3 col-sm-6">
             <input type="hidden" value="<?= $comment['id']?>" name="id_com"/>
             <div class="form-group">
                 <label for="author">Nom ou pseudo:</label> 
-                <input name="author" id="author" type=text value="<?= $comment['author'];?>" required="">
+                <input name="author" id="author" type=text value="<?= $comment['author'];?>" required="" class="form-control">
             </div>
             <div class="form-group">
                 <label for="comment">Modifier le commentaire :</label> 
-                <textarea name="comment" id="comment" required=""><?= $comment['comment'];?></textarea>
+                <textarea name="comment" id="comment" required="" class="form-control" rows="5"><?= $comment['comment'];?></textarea>
+                <p class="help-block">Vous pouvez agrandir la zone de texte</p>
             </div>
-            <input type="submit" value="Enregistrer les modifications du commentaire">
+            <input type="submit" value="Enregistrer les modifications du commentaire" class="confirmCom">
         </form>
     </div>
 </div>
